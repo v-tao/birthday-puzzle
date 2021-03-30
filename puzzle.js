33aabd360f4908f3e63ebfb727d63e9b69cd873f
Vivian Tao <vivian0tao@gmail.com>
2021-03-30 10:15:48 -0400

    switch puzzle component to function

--- puzzle.js
+++ puzzle.js
@@ -1,39 +1,16 @@
 "use strict";
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Puzzle = function (_React$Component) {
-    _inherits(Puzzle, _React$Component);
-
-    function Puzzle(props) {
-        _classCallCheck(this, Puzzle);
-
-        return _possibleConstructorReturn(this, (Puzzle.__proto__ || Object.getPrototypeOf(Puzzle)).call(this, props));
-    }
-
-    _createClass(Puzzle, [{
-        key: "render",
-        value: function render() {
-            return React.createElement(
-                "div",
-                null,
-                React.createElement(
-                    "h1",
-                    null,
-                    "Puzzle"
-                )
-            );
-        }
-    }]);
-
-    return Puzzle;
-}(React.Component);
+function Puzzle(props) {
+    return React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            "Test"
+        )
+    );
+}
 
 var puzzleContainer = document.querySelector("#puzzle");
 ReactDOM.render(React.createElement(Puzzle, null), puzzleContainer);
--- src/puzzle.js
+++ src/puzzle.js
@@ -1,18 +1,10 @@
 "use strict"
-class Puzzle extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>
-                    Puzzle
-                </h1>
-            </div>
-        )
-    }
+function Puzzle(props) {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
 }
 
 const puzzleContainer = document.querySelector("#puzzle");
